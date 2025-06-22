323c6323f37048fa7b889df0e1048ee65887bf4a
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2025-06-22 22:40:42 +0900

    feat: add biome configuration and update package dependencies; refactor code formatting across multiple files

--- lib/constructs/network.ts
+++ lib/constructs/network.ts
@@ -3,27 +3,27 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 import type * as cdk from "aws-cdk-lib";
 
 export class NetworkConstruct extends Construct {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+	constructor(scope: Construct, id: string) {
+		super(scope, id);
 
-    const vpc = new ec2.Vpc(this, "sample-vpc", {
-      vpcName: "sample-vpc",
-      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
-      maxAzs: 2,
-      enableDnsSupport: true,
-      enableDnsHostnames: true,
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: "public",
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        {
-          cidrMask: 24,
-          name: "private-isolated",
-          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-        },
-      ],
-    });
-  }
+		const vpc = new ec2.Vpc(this, "sample-vpc", {
+			vpcName: "sample-vpc",
+			ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+			maxAzs: 2,
+			enableDnsSupport: true,
+			enableDnsHostnames: true,
+			subnetConfiguration: [
+				{
+					cidrMask: 24,
+					name: "public",
+					subnetType: ec2.SubnetType.PUBLIC,
+				},
+				{
+					cidrMask: 24,
+					name: "private-isolated",
+					subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+				},
+			],
+		});
+	}
 }
--- lib/stack/cdk-ops-sample-stack.ts
+++ lib/stack/cdk-ops-sample-stack.ts
@@ -1,13 +1,13 @@
 import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
+import type { Construct } from "constructs";
 import { LambdaConstruct, NetworkConstruct } from "../constructs";
 
 export class CdkOpsSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambdaConstruct = new LambdaConstruct(this, "LambdaConstruct");
+    const _lambdaConstruct = new LambdaConstruct(this, "LambdaConstruct");
 
-    const networkConstruct = new NetworkConstruct(this, "NetworkConstruct");
+    const _networkConstruct = new NetworkConstruct(this, "NetworkConstruct");
   }
 }
