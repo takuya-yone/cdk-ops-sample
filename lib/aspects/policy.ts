b7f671d1582b370b3619abac87372db2dbd8c35b
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2025-06-23 08:56:38 +0900

    feat: remove unused checkLambda function from MyResourceChecker and add pnpm workspace configuration for esbuild

--- lib/aspects/policy.ts
+++ lib/aspects/policy.ts
@@ -1,11 +1,9 @@
 import type { IAspect } from "aws-cdk-lib";
 import type { IConstruct } from "constructs";
-import { checkLambda } from "./lambda";
 import { checkVpc } from "./vpc";
 
 export class MyResourceChecker implements IAspect {
   public visit(node: IConstruct): void {
     checkVpc(node);
-    checkLambda(node);
   }
 }
