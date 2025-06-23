e7e467cfc400dd2ae149e06e1b5b602fcd58a5a8
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2025-06-24 05:03:56 +0900

    Refactor code structure for improved readability and maintainability

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import boto3
+import pytest
+from moto import mock_aws
+import os
+
+
+@pytest.fixture()
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+
+
+@pytest.fixture()
+def s3_client(aws_credentials):
+    """
+    Return a mocked S3 resource client
+    """
+    with mock_aws():
+        yield boto3.client("s3")
+
+
+@pytest.fixture()
+def create_s3_bucket(s3_client):
+    """
+    Create a mocked S3 bucket
+    """
+    s3_client.create_bucket(Bucket="test-bucket-1")
+    s3_client.create_bucket(Bucket="test-bucket-2")
---
+++ tests/test_list_s3.py
@@ -0,0 +1,14 @@
+from moto import mock_aws
+
+
+@mock_aws
+def test_lambda_handler(mocker, create_s3_bucket):
+    """
+    handlerのテスト
+    """
+    from src.function.list_s3 import lambda_handler
+
+    res = lambda_handler({}, {})
+    
+    assert res['statusCode'] == 200
+    assert res['body'] == 'Hello from Lambda!'
