b244b1d61636db7d2825ac102d6aba9bc48339fb
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2025-07-07 20:20:39 +0900

    feat: add jsonc-parser dependency and update jest.config.js for module name mapping

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { readFileSync } = require("fs");
+const { parse } = require("jsonc-parser");
+// tsconfig.jsonからpathを取得
+// https://zenn.dev/no4_dev/articles/15ba046e245ba090a3a3-2
+const { compilerOptions } = parse(readFileSync("tsconfig.json").toString());
+const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" });
+
 module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/tests"],
@@ -6,7 +14,5 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest",
   },
   globalSetup: "<rootDir>/tests/setup-env.ts",
-  moduleNameMapper: {
-    'nodejs-layer': '<rootDir>/src/layer/nodejs-layer',
-  },
+  moduleNameMapper:moduleNameMapper
 };
