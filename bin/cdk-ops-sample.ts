8f5f64dec808f7f3eb5b78e314bbb416efb5d231
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2025-07-07 22:54:08 +0900

    refactor: rename MyResourceChecker to ComplianceChecker for clarity

--- bin/cdk-ops-sample.ts
+++ bin/cdk-ops-sample.ts
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib"
-import { MyResourceChecker } from "../lib/aspects/policy"
 import { CdkOpsSampleStack } from "../lib/stack/cdk-ops-sample-stack"
 
 const app = new cdk.App()
 
 new CdkOpsSampleStack(app, "CdkOpsSampleStack", {})
 
-// cdk.Aspects.of(app).add(new MyResourceChecker());
+// cdk.Aspects.of(app).add(new ComplianceChecker());
 
 cdk.Tags.of(app).add("Project", "cdk-ops-sample")
--- lib/aspects/policy.ts
+++ lib/aspects/policy.ts
@@ -2,7 +2,7 @@ import type { IAspect } from "aws-cdk-lib"
 import type { IConstruct } from "constructs"
 import { checkVpc } from "./vpc"
 
-export class MyResourceChecker implements IAspect {
+export class ComplianceChecker implements IAspect {
   public visit(node: IConstruct): void {
     checkVpc(node)
   }
